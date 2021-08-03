e8f64f3c4832a1fce0ebbbb405e2050b96435a15
ttuluk <ttasya.k@gmail.com>
2021-08-03 22:00:51 +0300

    add task-01

---
+++ js/task-01.js
@@ -0,0 +1,25 @@
+// Напиши скрипт, который выполнит следующие операции:
+// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item. 
+// Получится 'В списке 3 категории.'.
+
+const elem = document.querySelector('ul#categories');
+const listItem = elem.querySelectorAll('h2');
+const itemLength = listItem.length;
+console.log(`В списке ${itemLength} категории.`);
+
+// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка 
+// элемента(тега h2) и количество элементов в категории(всех вложенных в него элементов li).
+
+// Например для первой категории получится:
+
+// Категория: Животные
+// Количество элементов: 4
+
+const itemEl = elem.querySelectorAll('li.item');
+const title = itemEl.forEach(element => {
+
+        const titleEl = element.querySelector('h2');
+        const liItem = element.querySelectorAll('li');
+    console.log('Категория:', titleEl.textContent)
+    console.log('Количество элементов:', liItem.length);
+});
